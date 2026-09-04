180f251e5ba7b9baf5bb93f374e54c5e2ceb22e6
vulchevd <77018457+vulchevd@users.noreply.github.com>
2026-09-04 03:44:30 +0300

    Update main.tf

--- infra/terraform/modules/rds/main.tf
+++ infra/terraform/modules/rds/main.tf
@@ -63,7 +63,7 @@ resource "aws_secretsmanager_secret_version" "db" {
 resource "aws_db_instance" "this" {
   identifier     = var.name
   engine         = "postgres"
-  engine_version = "16.4"
+  engine_version = "16"
   instance_class = var.instance_class
 
   allocated_storage     = 20
